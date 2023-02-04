350ef505ab384c31c8dcf703c6813679999e9eaf
nodirxakimov <nodirxakimov@yandex.ru>
2023-02-04 14:13:54 +0500

    Translation button mistake fixed

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -57,7 +57,7 @@
 
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
-            <button
+            <button @click="toggleLang"
               class="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 flex ali"
               type="button" style="transition: all 0.15s ease 0s;">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -66,8 +66,8 @@
                   d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
               </svg>
               <div class="h-5">
-                <nuxt-link v-if="uzLang" :to="switchLocalePath('uz')" @click="toggleLang">Uzbek</nuxt-link>
-                <nuxt-link v-else :to="switchLocalePath('jp')" @click="toggleLang">Japanese</nuxt-link>
+                <nuxt-link v-if="uzLang" :to="switchLocalePath('uz')">Uzbek</nuxt-link>
+                <nuxt-link v-else :to="switchLocalePath('jp')">Japanese</nuxt-link>
               </div>
             </button>
           </li>
